409bdcd77e68914d4c7e28e1d0e7c2d529b43646
Afonso Raposo <afonso.raposo@paddle.com>
2025-12-10 21:47:34 +0000

    feat: day 10 part 2

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/afonsocraposo/advent-of-code
 
-go 1.22.6
+go 1.24.2
+
+require github.com/draffensperger/golp v0.0.0-20250721104811-2d405f0b4e68
--- internal/2025/day10/day10.go
+++ internal/2025/day10/day10.go
@@ -2,10 +2,13 @@ package day10
 
 import (
 	"fmt"
+	"math"
+	"slices"
 	"strings"
 
 	"github.com/afonsocraposo/advent-of-code/internal/utils/matrix"
 	"github.com/afonsocraposo/advent-of-code/internal/utils/runner"
+	"github.com/draffensperger/golp"
 )
 
 const year = 2025
@@ -15,9 +18,9 @@ func Main() {
 	r := runner.New(year, day, part1, part2)
 	r.TestPart1(1, 1)
 	r.RunPart1(1)
-	//
-	// r.TestPart2(1, 2)
-	// r.RunPart2(1)
+
+	r.TestPart2(1, 2)
+	r.RunPart2(1)
 }
 
 func parseIndicator(indicator string) matrix.Vector {
@@ -31,14 +34,26 @@ func parseIndicator(indicator string) matrix.Vector {
 	return v
 }
 
-func parseButton(button string, length int) matrix.Vector {
-	v, err := matrix.ParseVector(button[1:len(button)-1], ",")
+func parseButton(buttons string, length int) matrix.Vector {
+	v, err := matrix.ParseVector(buttons[1:len(buttons)-1], ",")
 	if err != nil {
 		panic(err)
 	}
 	result := matrix.NewEmptyVector(length)
 	for _, i := range v.Values {
 		result.Set(i, 1)
+	}
+	return result
+}
+
+func parseJoltage(joltage string, length int) matrix.Vector {
+	v, err := matrix.ParseVector(joltage[1:len(joltage)-1], ",")
+	if err != nil {
+		panic(err)
+	}
+	result := matrix.NewEmptyVector(length)
+	for i, joltage := range v.Values {
+		result.Set(i, joltage)
 	}
 	return result
 }
@@ -107,10 +122,65 @@ func part1(lines []string) string {
 	return fmt.Sprintf("%d", solution)
 }
 
+func solve(mat matrix.Matrix, result matrix.Vector) int {
+	_, n := mat.Size() // number of variables
+	lp := golp.NewLP(0, n)
+
+	// minimize a+b+c+d...
+	obj := slices.Repeat([]float64{-1}, n)
+	lp.SetObjFn(obj)
+	lp.SetMaximize()
+
+	// add constraints (each row of the matrix)
+	for i, row := range mat.Rows {
+		fl := make([]float64, row.Size())
+		for j, v := range row.Values {
+			fl[j] = float64(v)
+		}
+		lp.AddConstraint(fl, golp.EQ, float64(result.Values[i]))
+	}
+
+	// non negative variables
+	for i := 1; i < n; i++ {
+		lp.SetBounds(i, 0, 10000) // variable i has lower bound 0
+		lp.SetInt(i, true)
+	}
+
+	lp.Solve()
+	sol := 0
+	for _, v := range lp.Variables() {
+		sol += int(math.Round(v))
+	}
+
+	return sol
+}
+
 func part2(lines []string) string {
 	solution := 0
 
-	// TODO
+	for _, line := range lines {
+		parts := strings.Split(line, " ")
+		indicatorString := parts[0]
+		buttonsString := parts[1 : len(parts)-1]
+		joltageString := parts[len(parts)-1]
+
+		indicator := parseIndicator(indicatorString)
+
+		buttons := make([]matrix.Vector, len(buttonsString))
+		for i, b := range buttonsString {
+			button := parseButton(b, indicator.Size())
+			buttons[i] = button
+		}
+		joltage := parseJoltage(joltageString, indicator.Size())
+
+		m := matrix.NewMatrix(buttons)
+		t, err := m.Transpose()
+		if err != nil {
+			panic(err)
+		}
+		result := solve(*t, joltage)
+		solution += result
+	}
 
 	return fmt.Sprintf("%d", solution)
 }
